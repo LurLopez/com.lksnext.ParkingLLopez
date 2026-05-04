4d0aaab6493dc2e47af7b7101f0079a4da8bdbf8
Lur Lopez <lurlopez13@gmail.com>
2026-05-05 00:36:46 +0200

    prueba 7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     // El "apply false" es OBLIGATORIO aquí arriba
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("org.sonarqube") version "5.0.0.4638" apply false
+
 }
